ce7d1df54a3204173e45cd916e1424e869ccd0ef
Mohammed Salim Kapasi <94778301+CMDR-VanderWaal@users.noreply.github.com>
2022-11-01 23:56:07 +0400

    Update main.py

--- MarkerDetection/main.py
+++ MarkerDetection/main.py
@@ -16,30 +16,34 @@ while True:
 
     results = aruco.detectMarkers(frame, marker_dict, parameters=params)
     corners, ids, _ = results
-    print(corners)
+    #print(corners)
     index = 0
 
     try:
-        for index in ids.flatten():
-            corner = corners[index][0]
+        try:
+            for index in ids.flatten():
+                corner = corners[index][0]
+                print(corner)
+        except AttributeError:
+            print("No ArucoMarker Detected in frame ! ")
+            continue
 
-            # corner - is the List of Cordinates of each Corner Pixel of an Aruco Marker
-            for i in range(0, len(corner)):
-                # print(corner)
-                p1 = corner[i]
-                j = i+1
-                j = j % len(corner)
-                p2 = corner[j]
+        """corner - is the List of Cordinates of each Corner Pixel of an Aruco Marker"""
+        for i in range(0, len(corner)):
+            # print(corner)
+            p1 = corner[i]
+            j = i+1
+            j = j % len(corner)
+            p2 = corner[j]
 
-                p1 = np.array(p1, dtype=np.int32)
-                p2 = np.array(p2, dtype=np.int32)
-                # print("points", p1, p2)
-                cv.line(frame, p1, p2, (0, 255, 0), 5)
+            p1 = np.array(p1, dtype=np.int32)
+            p2 = np.array(p2, dtype=np.int32)
+            # print("points", p1, p2)
+            cv.line(frame, p1, p2, (0, 255, 0), 5)
 
-            cv.namedWindow("Image", cv.WINDOW_NORMAL)
-            cv.imshow("Image", frame)
-            key = cv.waitKey(1)
-
+        cv.namedWindow("Image", cv.WINDOW_NORMAL)
+        cv.imshow("Image", frame)
+        key = cv.waitKey(1)
     except IndexError:
         continue
     if key == ord("q"):
